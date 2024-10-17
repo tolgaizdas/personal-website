6279909fd7514e1b410fc8ef29feaa836db5d2b3
Tolga İzdaş <tolgaizdas@gmail.com>
2024-10-18 02:12:28 +0300

    added columns for sections with multiple subsections

--- src/components/section.js
+++ src/components/section.js
@@ -1,9 +1,11 @@
-// components/Section.js
 import Subsection from './subsection';
 
 const Section = ({ header, subsections }) => {
   // Sort subsections by date (assuming date is in a sortable format like ISO)
   const sortedSubsections = [...subsections].sort((a, b) => new Date(b.date) - new Date(a.date));
+
+  // Determine the number of columns based on the number of subsections
+  const gridCols = subsections.length > 2 ? 'lg:grid-cols-3' : subsections.length === 2 ? 'lg:grid-cols-2' : 'lg:grid-cols-1';
 
   return (
     <div className="mb-8">
@@ -11,9 +13,11 @@ const Section = ({ header, subsections }) => {
         {header}
       </h1>
       {sortedSubsections.length > 0 ? (
-        sortedSubsections.map((subsection, index) => (
-          <Subsection key={index} {...subsection} />
-        ))
+        <div className={`grid gap-4 ${gridCols}`}>
+          {sortedSubsections.map((subsection, index) => (
+            <Subsection key={index} {...subsection} />
+          ))}
+        </div>
       ) : (
         <p className="text-gray-600 dark:text-gray-400">
           No subsections available.
