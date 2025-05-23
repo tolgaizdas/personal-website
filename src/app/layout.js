a483a1c9f7177db282de38124517fe45c5bb1229
Tolga İzdaş <tolgaizdas@gmail.com>
2025-05-23 20:12:49 +0300

    style: adjust body margin for improved layout responsiveness

--- src/app/layout.js
+++ src/app/layout.js
@@ -11,7 +11,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en" className="dark">
-      <body className={inter.className}>{children}</body>
+      <body className={`${inter.className} mx-0 md:mx-[10%]`}>{children}</body>
     </html>
   );
 }
