b2d3c41dc448abd410d0417aa2c615cefd03279a
Tolga İzdaş <tolgaizdas@gmail.com>
2026-09-24 02:27:13 -0700

    Personalize homepage introduction copy

--- src/components/layout/Introduction.jsx
+++ src/components/layout/Introduction.jsx
@@ -1,15 +1,13 @@
 import Image from "next/image";
 
 const Introduction = ({ information }) => {
-  const firstName = information.name.split(" ")[0];
-
   return (
     <section className="home-intro" aria-labelledby="intro-title">
       <div className="home-intro-content">
-        <h1 id="intro-title">I am <em>{firstName}</em>.</h1>
+        <h1 id="intro-title">Hi, I&apos;m <em>Tolga</em>!</h1>
         <p className="home-deck">
-          A computer science master&apos;s student at the University of Southern
-          California.
+          I&apos;m a computer science master&apos;s student at the University of
+          Southern California.
         </p>
 
         <div className="home-actions" aria-label="Get in touch">
